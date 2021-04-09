0c818e2db4682a3aa44faa4af6856cc3b292d215
Cecman <jane.trajkoski@gmail.com>
2021-04-09 14:41:12 +0200

    added post route and handler for movies

--- api/handlers/movieHandler.js
+++ api/handlers/movieHandler.js
@@ -1,4 +1,5 @@
 const { Movie, validator } = require("../../src/db/models/movie");
+const { Genre } = require("../../src/db/models/genre");
 
 const getMovieHandler = async (req, res) => {
   const movies = await Movie.find().select("-_id -__v");
@@ -10,7 +11,21 @@ const getMovieHandler = async (req, res) => {
   res.send(movies);
 };
 const createMovieHandler = async (req, res) => {
-  console.log("create handler");
+  const { error } = validator(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const newMovie = new Movie({
+    title: req.body.title,
+    genre: new Genre({ genre: req.body.genre }),
+    stock: req.body.stock,
+    rentalPrice: req.body.rentalPrice,
+    rentalRate: req.body.rentalRate,
+  });
+
+  newMovie.save();
+  res.send(newMovie);
 };
 const updateMovieHandler = async (req, res) => {
   console.log("create handler");
