bc46a8b7aafdca69cbe0ef9c5d2f315169cb6b19
Jane Trajkoski <janet@xgate.io>
2021-05-14 13:45:51 +0200

    Added login api

--- api/handlers/loginHandler.js
+++ api/handlers/loginHandler.js
@@ -1,8 +1,8 @@
 const { User } = require("../../src/db/models/user");
-const validateLogin = require("../../middleware/authUser");
+const validateUser = require("../../middleware/authUser");
 
 const loginUser = async (req, res) => {
-  const { error } = validateLogin(req.body);
+  const { error } = validateUser(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
